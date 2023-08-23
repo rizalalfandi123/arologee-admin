678079a54266e6f99d9a2df44cef34885b30c0ce
Rizal Alfandi <rizalalfandi123456789@gmail.com>
2023-08-23 20:26:00 +0700

    fix autofill input

--- src/app/[lang]/(dashboard)/sidebar.tsx
+++ src/app/[lang]/(dashboard)/sidebar.tsx
@@ -1,9 +1,10 @@
 import "server-only";
 
 import Image from "next/image";
-import { ChevronsLeft, Home, DollarSign, User2, Watch } from "lucide-react";
+import { Home, DollarSign, User2, Watch, MoreVerticalIcon } from "lucide-react";
 import { type Dictionaries } from "@/lib/types";
 import SidebarItem, { type ISidebarItem } from "./sidebar-item";
+import UserOptions from "./user-options";
 
 interface SidebarProps {
   dictionaries: Dictionaries["sidebar"];
@@ -48,19 +49,37 @@ export default function Sidebar(props: SidebarProps) {
   ];
 
   return (
-    <div className="bg-sidebar p-0 h-full w-full md:px-6 md:py-8">
-      <div className="w-full flex justify-between items-center mb-6">
-        <Image src="/images/brand.svg" width={88} height={38} alt="brand" />
+    <div className="bg-sidebar p-0 h-full flex flex-col justify-between w-full md:px-4 md:py-6">
+      <div className="flex flex-col">
+        <div className="w-full mb-6">
+          <Image src="/images/brand.svg" width={88} height={38} alt="brand" />
+        </div>
 
-        {/* <ChevronsLeft /> */}
+        <h3 className="text-sidebar-foreground mb-4">
+          {dictionaries.settings}
+        </h3>
+
+        <div className="space-y-3">
+          {sidebarItems.map((item) => {
+            return <SidebarItem {...item} key={item.label} />;
+          })}
+        </div>
       </div>
 
-      <h3 className="text-sidebar-foreground mb-4">{dictionaries.settings}</h3>
+      <div className="flex justify-between w-full">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/images/avatar.png"
+            className="rounded-full"
+            alt="avatar"
+            width={32}
+            height={32}
+          />
 
-      <div className="space-y-3">
-        {sidebarItems.map((item) => {
-          return <SidebarItem {...item} key={item.label} />;
-        })}
+          <p>Mclaren Steve</p>
+        </div>
+
+        <UserOptions/>
       </div>
     </div>
   );
---
+++ src/app/[lang]/(dashboard)/user-options.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components";
+import { LogOutIcon, MoreVerticalIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
+export default function UserOptions() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    Cookies.remove("auth");
+    router.push("/login");
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon" variant="ghost">
+          <MoreVerticalIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuItem onClick={handleLogout} className="text-primary">
+          <LogOutIcon className="w-5 h-5 mr-2" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
